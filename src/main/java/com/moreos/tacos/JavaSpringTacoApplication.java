e61f39f2b70955d4028b6c212807ea93f4b59c59
Jennine Serra <jserra@student.21-school.ru>
2023-02-15 22:34:50 +0700

    second commit

---
+++ src/main/java/com/moreos/tacos/JavaSpringTacoApplication.java
@@ -0,0 +1,13 @@
+package com.moreos.tacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringTacoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringTacoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/moreos/tacos/JavaSpringTacoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moreos.tacos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringTacoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
